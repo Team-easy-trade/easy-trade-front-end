81ebefe1e9af68b3cf6c1432669042c5cd067a98
Nicholas Babcock <Nicholas.Babcock@TLGcohort.com>
2021-07-20 09:06:39 -0700

    created roots(footer,header) in index, categories section w/ search bar,and header.js finished

---
+++ app/header.js
@@ -0,0 +1,32 @@
+
+const headRoot = document.getElementById("header")
+
+headRoot.innerHTML = 
+`
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">Easy-Trade</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">About us</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Listings</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>
+</div>
+<form class="form-inline">
+<button class="btn btn-outline-success" type="button">Sign In</button>
+<button class="btn btn-sm btn-outline-secondary" type="button">Sign Up</button>
+</form>
+</nav>
+`
